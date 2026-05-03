fdb812af2671cfc1049bdb0da6a16df49d6b7148
Adeife <adeife@adeifes-air.home>
2026-05-03 15:38:12 -0400

    Added database connection

---
+++ db/connection.py
@@ -0,0 +1,42 @@
+import sqlite3
+
+
+sql_statements = [
+    """CREATE TABLE IF NOT EXISTS users (
+            user_id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL,
+            email TEXT NOT NULL,
+            password TEXT NOT NULL,
+            date DATE
+        );
+    """,
+
+    """CREATE TABLE IF NOT EXISTS items (
+            item_id INTEGER PRIMARY KEY,
+            user_id INTEGER,
+            title TEXT NOT NULL,
+            description TEXT,
+            completed BOOLEAN NOT NULL DEFAULT 0,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            FOREIGN KEY (user_id) REFERENCES users(user_id)
+        );
+    """
+]
+
+try:
+    with sqlite3.connect("to-do.db") as conn:
+        print(f"Opened SQLite database with version {sqlite3.sqlite_version} successfully.")
+
+        cursor = conn.cursor()
+
+        for statement in sql_statements:
+            cursor.execute(statement)
+
+        conn.commit()
+
+        print("Tables created succesfully.")
+
+        conn.execute("PRAGMA foreign_keys = ON")
+        
+except sqlite3.OperationalError as e:
+    print("Failed to connect to database:", e)
