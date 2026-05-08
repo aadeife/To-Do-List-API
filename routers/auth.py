83ccd115173f63a1115ce1959471095c21077068
aadeife <aadeifea@gmail.com>
2026-05-07 21:35:46 -0400

    updated login and register routes

--- routers/auth.py
+++ routers/auth.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, HTTPException, Depends
-from models import UserCreate, UserResponse, UserUpdate, UserLogin
+from models import UserCreate, UserResponse, UserUpdate, UserLogin, TokenResponse
 import sqlite3
 from db import connection
 from pwdlib import PasswordHash
+from jose import jwt
 
 router = APIRouter()
 password_hash = PasswordHash.recommended()
@@ -22,11 +23,11 @@ def user_registration(user: UserCreate, db = Depends(connection.get_db)):
         cursor.execute(
             check_user, (user.email,)
         )
-        new_user = cursor.fetchone()
+        existing_user = cursor.fetchone()
     except sqlite3.OperationalError as e:
         raise HTTPException(status_code=500, detail="Database error")
     
-    if new_user:
+    if existing_user:
         raise HTTPException(status_code=400, detail="Email already exists.")
     
     try:
@@ -39,6 +40,43 @@ def user_registration(user: UserCreate, db = Depends(connection.get_db)):
         user_id = cursor.lastrowid
     except sqlite3.OperationalError as e:
         raise HTTPException(status_code=500, detail="Database error")
+    
+    token = jwt.encode(a{"user_id": user_id}, 'secret', algorithm='HS256')
+
+    return TokenResponse(token=token)
+
+@router.post("/login")
+def user_login(user: UserLogin, db = Depends(connection.get_db)):
+    # check user exists
+    check_user = """
+                    SELECT * FROM users WHERE email = ?
+                 """
+    
+    cursor = db.cursor()
+    
+    try:
+        cursor.execute(
+            check_user, (user.email,)
+        )
+        existing_user = cursor.fetchone()
+    except sqlite3.OperationalError as e:
+        raise HTTPException(status_code=500, detail="Database error")
+    
+    if not existing_user:
+        raise HTTPException(status_code=401, detail="Incorrect email or password.")
+    
+    # check password
+    real_pwd = existing_user["password"]
+    user_id = existing_user["user_id"]
+
+    if not password_hash.verify(user.password, real_pwd):
+        raise HTTPException(status_code=401, detail="Incorrect email or password.")
+    
+    token = jwt.encode({"user_id": user_id}, 'secret', algorithm='HS256')
+
+    return TokenResponse(token=token)
+    
+
 
 
 
